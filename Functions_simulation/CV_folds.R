c593718626c610cd14f7fd5b6bb318b2a3715282
Paulina von Stackelberg <34370704+paulinavonstackelberg@users.noreply.github.com>
2021-05-15 20:03:20 +0200

    Update CV_folds.R

--- Functions_simulation/CV_folds.R
+++ Functions_simulation/CV_folds.R
@@ -17,7 +17,7 @@ CV_fold <- function(n_param, data, k_def){
         folds <- createFolds(data[,(n_param+1)], k= k_def, list = TRUE, returnTrain = FALSE)
       }
     }
-  } else{
+  } else{ # this is for k=5
     if((sum(as.numeric(data[folds[[1]], (n_param+1)])-1) == 0) ||(sum(as.numeric(data[folds[[2]], (n_param+1)])-1) == 0) ||(sum(as.numeric(data[folds[[3]], (n_param+1)])-1) == 0)
        || (sum(as.numeric(data[folds[[4]], (n_param+1)])-1) == 0) || (sum(as.numeric(data[folds[[5]], (n_param+1)])-1) == 0)){
       while((sum(as.numeric(data[folds[[1]], (n_param+1)])-1) == 0) ||(sum(as.numeric(data[folds[[2]], (n_param+1)])-1) == 0) ||(sum(as.numeric(data[folds[[3]], (n_param+1)])-1) == 0)
@@ -32,30 +32,29 @@ CV_fold <- function(n_param, data, k_def){
 
 CV_fold_extract <- function(folds, data, nsim, k_def){
   
-  dat_split <- list()
+  dat_split <- list() # storage list
   
-  if (k_def == 3){
+  if (k_def == 3){ # extract the CV folds as defined
     
     for(i in 1:nsim){
-      fold1 <- data[[i]][folds[[i]][["Fold1"]],]
-      fold2 <- data[[i]][folds[[i]][["Fold2"]],]
-      fold3 <- data[[i]][folds[[i]][["Fold3"]],]
+      fold1 <- data[[i]][folds[[i]][["Fold1"]],] # fold 1
+      fold2 <- data[[i]][folds[[i]][["Fold2"]],] # fold 2
+      fold3 <- data[[i]][folds[[i]][["Fold3"]],] # fold 3
       data_folds <- list("fold1" = fold1, "fold2" = fold2, "fold3" = fold3)
       dat_split[[i]] <- data_folds
     }
-  } else{
+  } else{ # for k=5
     for(i in 1:nsim){
       fold1 <- data[[i]][folds[[i]][["Fold1"]],]
       fold2 <- data[[i]][folds[[i]][["Fold2"]],]
       fold3 <- data[[i]][folds[[i]][["Fold3"]],]
       fold4 <- data[[i]][folds[[i]][["Fold4"]],]
       fold5 <- data[[i]][folds[[i]][["Fold5"]],]
-      
+     
       data_folds <- list("fold1" = fold1, "fold2" = fold2, "fold3" = fold3, "fold4" = fold4, "fold5" = fold5)
       dat_split[[i]] <- data_folds
     }
   }
-  
   return(dat_split)
 }
 
@@ -63,11 +62,11 @@ CV_fold_extract <- function(folds, data, nsim, k_def){
 CV_fold_extract_imp <- function(folds_defi, con1_dat, nsim, k_def){
   dat_split_it <- list()
   for(i in 1:nsim){
-    fold_defi <- folds_defi[[i]]
-    data_imp_def <- con1_dat[[i]]
+    fold_defi <- folds_defi[[i]] # storage list
+    data_imp_def <- con1_dat[[i]] # storage list
     dat_split <- list()
-    for(j in 1:5){
-      if (k_def == 3){
+    for(j in 1:5){ # run through the five imputed datasets
+      if (k_def == 3){ # getting the 3 CV folds
         fold1 <- data_imp_def[[j]][fold_defi[["Fold1"]],]
         fold2 <- data_imp_def[[j]][fold_defi[["Fold2"]],]
         fold3 <- data_imp_def[[j]][fold_defi[["Fold3"]],]
@@ -84,5 +83,5 @@ CV_fold_extract_imp <- function(folds_defi, con1_dat, nsim, k_def){
     }
     dat_split_it[[i]] <- dat_split
   }
-  return(dat_split_it)
+  return(dat_split_it) # return the splits per imputed dataset
 }
