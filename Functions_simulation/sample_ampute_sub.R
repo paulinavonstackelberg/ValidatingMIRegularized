213154eab7549ac16ea06d81c57cfbb996071c49
Paulina von Stackelberg <34370704+paulinavonstackelberg@users.noreply.github.com>
2021-05-15 20:06:58 +0200

    Update sample_ampute_sub.R

--- Functions_simulation/sample_ampute_sub.R
+++ Functions_simulation/sample_ampute_sub.R
@@ -9,7 +9,6 @@ library(mice)
 
 sample_sub <- function(n, data_comp){
   data_sub_row <- sample(nrow(data_comp), n, replace = FALSE) # sampling from huge dataset without replacement
-  #length(unique(data_sub_row))
   data_sub <- data_comp[data_sub_row,]
   data_ext <- data_comp[-data_sub_row, ] # this is the 'external' dataset
   return(list("data_sub" = data_sub, "data_ext" = data_ext))
@@ -18,22 +17,22 @@ sample_sub <- function(n, data_comp){
 sample_sub_list <- function(nsim, data_comp, n){
   samp_list <- list()
   for (i in 1:nsim){
-    samp_list[[i]] <- sample_sub(n=n, data_comp=data_comp)
+    samp_list[[i]] <- sample_sub(n=n, data_comp=data_comp) # do this (subsample) for each of the simulation iterations
   }
   return(samp_list)
 }
 
 
 ampute_sub <- function(data_sub, mis_pattern, mis_prop, mis_mech = c("MCAR", "MAR"), n_param){ 
-  if(mis_mech == "MAR"){
+  if(mis_mech == "MAR"){ # MAR missingness
     amputing <- ampute(data_sub, prop = mis_prop, patterns = mis_pattern, mech = "MAR")
-    amputed_dat <-amputing$amp
+    amputed_dat <-amputing$amp # amputed data
     # re-factor and return amputed data
     amputed_dat[,(n_param+1)] <- amputed_dat[,(n_param+1)] - 1
     amputed_dat[,(n_param+1)] <- as.factor(amputed_dat[,(n_param+1)])
-  } else{
+  } else{ # MCAR missingness
     amputing <- ampute(data_sub, prop = mis_prop, patterns = mis_pattern, mech = "MCAR")
-    amputed_dat <-amputing$amp
+    amputed_dat <-amputing$amp # amputed data
   }
   
   return(amputed_dat)
